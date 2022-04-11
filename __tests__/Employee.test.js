e44d028f9f9cdc409af2ea76c9b83cce3482c85e
Michael Kendrick <michael@locumsmart.net>
2022-04-11 14:18:24 -0500

    begin

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,17 @@
+class Employee {
+  constructor(name) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+
+  getName() {
+    if (name === "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+module.exports = Employee;
