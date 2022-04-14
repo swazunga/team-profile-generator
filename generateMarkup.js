ea39be42bc7595da76b60bbf694ccf7a2594e123
Michael Kendrick <michael@locumsmart.net>
2022-04-14 16:04:53 -0500

    to the page

--- generateMarkup.js
+++ generateMarkup.js
@@ -2,22 +2,35 @@ function teamMemberCard(data) {
   let divArray = [];
   for (let member of data) {
     let div = `
-    <div><h2>${member.name}</h2>
+    <div class="employee-cards">
+    <div class="container"><h2>${member.name}</h2>
     <h2>${member.getRole()}</h2>
-    <h3> ID: ${member.id}</h3>
-    <h3>Email: ${member.email}</h3>
-   
-    </div>
+    <h3> ID: ${member.getId()}</h3>
+    <h3>Email: ${member.getEmail()}</h3>
+       </div>
+       </div>
     `;
     divArray.push(div);
     console.log(member);
   }
+
+  // function teamMemberGithub(data) {
+  //   if (data.getRole() === Manager) {
+  //     `Office #: ${data.managerOffice}`;
+  //   } else if (data.getRole() === Engineer) {
+  //     `GitHub: ${data.github}`;
+  //   } else {
+  //     `School: ${data.school}`;
+  //   }
+  // }
+
   console.log(divArray);
   return divArray.join("");
 }
 
 function generateMarkup(data) {
   const teamCards = teamMemberCard(data);
+  // const lastLine = teamMemberGithub(data);
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -29,11 +42,14 @@ function generateMarkup(data) {
   </head>
   <body>
       <h1>My Team</h1>
-      ${teamCards}
+      <div class="all-cards">
+       ${teamCards}
+       ${lastLine}
+      </div>
   </body>
   </html>
   
-  ;`;
+  `;
 }
 
 module.exports = generateMarkup;
